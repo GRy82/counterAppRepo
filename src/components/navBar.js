03cb81a2c6aa31c9a1feb7320106b50895ec4f84
Gregory Ryan <gwryan@buffalo.edu>
2021-06-23 16:36:59 -0400

    navBar converted to stateless functional component.

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
 
-class NavBar extends Component {
-    render() { 
-        return ( 
-            <nav className="navbar navbar-light bg-light">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="">
-                        NavBar <span className="badge bg-pill bg-secondary">{this.props.count}</span>
-                    </a>
-                </div>
-            </nav>
-         );
-    }
+const NavBar = (props) => {
+    return ( 
+        <nav className="navbar navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="">
+                    NavBar <span className="badge bg-pill bg-secondary">{props.count}</span>
+                </a>
+            </div>
+        </nav>
+        );
 }
  
 export default NavBar;
